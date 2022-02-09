322c3b346de520a73ab98b5ec279c9c7c3a4dd30
Abinash <abinash261997@gmail.com>
2022-02-09 17:05:48 +0530

    complete registration setup

--- src/Register.js
+++ src/Register.js
@@ -71,7 +71,7 @@ function Register() {
                     onClick={signup}
                     className="btn btn-primary d-block w-100"
                 >
-                    Register
+                    Signin
                 </button>
             </div>
         </>
